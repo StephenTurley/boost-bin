980add115c0f471adaf501855f83644a0a45efad
Stephen Turley <turley714@gmail.com>
2016-06-19 20:05:56 -0400

    added embedded jetty, removed javax servlet

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-jetty-adapter "1.5.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler boost-bin.handler/app}
+	:main boost-bin.handler
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  {:uberjar {:aot :all}
+	 :dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
--- src/boost_bin/handler.clj
+++ src/boost_bin/handler.clj
@@ -1,11 +1,18 @@
 (ns boost-bin.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.adapter.jetty :refer :all]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:gen-class))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-defaults app-routes site-defaults))
+
+(defn -main
+  [& args]
+  (run-jetty
+    app
+    {:port 8080}))
