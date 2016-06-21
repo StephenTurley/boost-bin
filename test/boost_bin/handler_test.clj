faad21e52aa6c4a25897fd9906c6c0a5111bfdf9
Stephen Turley <turley714@gmail.com>
2016-06-21 19:24:42 -0400

    added testing for -main

--- test/boost_bin/handler_test.clj
+++ test/boost_bin/handler_test.clj
@@ -1,17 +1,36 @@
 (ns boost-bin.handler-test
   (:require [speclj.core :refer :all]
             [ring.mock.request :as mock]
-            [boost-bin.handler :refer :all]))
+            [boost-bin.handler :as underTest]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
 (describe "The routes"
   (describe "main route"
-    (let [response (app (mock/request :get "/"))]
+    (let [response (underTest/app (mock/request :get "/"))]
       (it "should return a 200"
           (should= (:status response) 200))
       (it "should return Hello World"
           (should= (:body response) "Hello World"))))
 
   (describe "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
+    (let [response (underTest/app (mock/request :get "/invalid"))]
       (it "should return a 400"
           (should= (:status response) 404)))))
+
+
+(describe "The app"
+  (it "should default to port 8080"
+    (with-redefs [run-jetty (fn [handler opts] [handler opts])]
+      (should= 8080 (->
+											(underTest/-main)
+											(nth 1)
+											(:port)))))
+	(it "should overide the port with first arg"
+		(with-redefs [run-jetty (fn [handler opts] [handler opts])]
+			(should= 8081 (->
+											(underTest/-main "8081")
+											(nth 1)
+											(:port)))))
+	(it "should set the app handler"
+		(with-redefs [run-jetty (fn [handler opts] [handler opts])]
+			(should= underTest/app (first (underTest/-main))))))
