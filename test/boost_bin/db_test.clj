37213a1d98dd5a4b0eafdd13549e9d66b5dfa3ad
Stephen Turley <turley714@gmail.com>
2016-07-07 14:35:00 -0400

    patch must be within the it call unless you use the around macro

--- test/boost_bin/db_test.clj
+++ test/boost_bin/db_test.clj
@@ -3,14 +3,16 @@
             [boost-bin.db :as underTest]
             [boost-bin.csv :as csv]
             [clojure.java.io :as io]
-            [clj-fakes.core :as f :include-macros true]))
+            [clj-fakes.core :as f]))
 
 (def test-data (io/resource "test/testdata.csv"))
 (def vector-map {:derp [:foo :bar]})
 
-(f/with-fakes
-  (describe "save data log"
-    (it "should return the csv as a map"
-    (f/patch! #'boost-bin.csv/as-vector-map (f/fake [[f/any?] vector-map]))
-      (should= vector-map
-            (underTest/save-data-log {:tempfile test-data})))))
+(describe
+  "save data log"
+  (it "should return the csv as a map"
+      (f/with-fakes
+        (f/patch! #'csv/as-vector-map (f/fake [[test-data] vector-map]))
+
+        (should= vector-map
+                 (underTest/save-data-log {:tempfile test-data})))))
