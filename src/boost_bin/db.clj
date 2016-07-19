f9c9aae076f9ee2d8820e61bb1e75e73c2a51792
Stephen Turley <turley714@gmail.com>
2016-07-18 21:04:40 -0400

    spiked out saving to local db

--- src/boost_bin/db.clj
+++ src/boost_bin/db.clj
@@ -1,6 +1,14 @@
 (ns boost-bin.db
-  (:require [boost-bin.csv :as csv]))
+  (:require [boost-bin.csv :as csv]
+						[monger.core :as mg]
+						[monger.collection :as mc])
+	(:import org.bson.types.ObjectId))
 
 (defn save-data-log
   [data]
-	(csv/as-vector-map (:tempfile data)))
+	(let [conn (mg/connect)
+				db (mg/get-db conn "boostbin")
+				id (ObjectId.)]
+		(mc/insert-and-return db "datalogs"
+							 {:id id, :data (csv/as-vector-map (:tempfile data))})
+		{:id (.toString id)}))
